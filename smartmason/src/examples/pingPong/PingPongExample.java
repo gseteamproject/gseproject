dc2dc89742c508797928060ad92a0a4b088187b3
Кирилл Киприянов <kinnder@mail.ru>
2016-04-27 01:46:22 +0300

    PinPongExample moved to its own package and build configuration replaced with Launcher class

---
+++ smartmason/src/examples/pingPong/PingPongExample.java
@@ -0,0 +1,18 @@
+package examples.pingPong;
+
+import jade.Boot;
+
+public class PingPongExample
+{
+	public static void main(String[] p_args)
+	{
+		System.out.println("Ping pong example started ...");		
+		
+		String[] parameters = new String[ 2 ];
+		parameters[ 0 ] = "-gui";
+		parameters[ 1 ] = "ping:examples.pingPong.PingAgent;";
+		parameters[ 1 ] += "pong:examples.pingPong.PongAgent;";
+
+		Boot.main( parameters );
+	}
+}
