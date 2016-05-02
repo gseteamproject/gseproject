a88d70859fa9430e69d7024f929ace4f7dcb53ef
Кирилл Киприянов <kinnder@mail.ru>
2016-05-02 14:44:12 +0400

    #5 simplified type names

--- smartmason/src/examples/bookTrading/BookTrading.java
+++ smartmason/src/examples/bookTrading/BookTrading.java
@@ -2,7 +2,8 @@ package examples.bookTrading;
 
 public class BookTrading
 {
-	public static String BOOK_SELLER_AGENT = "Seller-agent";
-	public static String BOOK_TRADING_SERVICES = "JADE-book-trading";
-	public static String BOOK_SELLING_SERVICE = "book-selling";
+	public static final String BOOK_SELLER_AGENT = "Seller";
+	public static final String BOOK_BUYER_AGENT = "Buyer";
+	public static final String BOOK_TRADING_SERVICES = "JADE-book-trading";
+	public static final String BOOK_SELLING_SERVICE = "book-selling";
 }
