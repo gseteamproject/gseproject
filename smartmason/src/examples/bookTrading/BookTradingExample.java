783acf45119b932b6f238600a02cf3de36686e8b
Кирилл Киприянов <kinnder@mail.ru>
2016-05-03 14:10:07 +0400

    #5 scenario extended with additional buyers

--- smartmason/src/examples/bookTrading/BookTradingExample.java
+++ smartmason/src/examples/bookTrading/BookTradingExample.java
@@ -17,7 +17,9 @@ public class BookTradingExample
 		
 		String[] parameters = new String[ 2 ];
 		parameters[ 0 ] = "-gui";
-		parameters[ 1 ] = "buyer1:examples.bookTrading.BookBuyerAgent(" + BookTitle.LORD_OF_THE_RINGS + ");";
+		parameters[ 1 ] = "reader:examples.bookTrading.BookBuyerAgent(" + BookTitle.LORD_OF_THE_RINGS + ");";
+		parameters[ 1 ] += "noviceProgrammer:examples.bookTrading.BookBuyerAgent(" + BookTitle.JAVA_TUTORIAL + ");";
+		parameters[ 1 ] += "advancedProgrammer:examples.bookTrading.BookBuyerAgent(" + BookTitle.JADE_PROGRAMMING_TUTORIAL + ");";
 		parameters[ 1 ] += "seller1:examples.bookTrading.BookSellerAgent;";
 		parameters[ 1 ] += "seller2:examples.bookTrading.BookSellerAgent;";
 
