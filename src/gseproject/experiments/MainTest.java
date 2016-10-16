9954e4d0b595f0f5290d73973a728424d941d07b
Risthart <razzak66@bk.ru>
2016-10-17 00:04:29 +0300

    Test testing :)

--- src/gseproject/experiments/MainTest.java
+++ src/gseproject/experiments/MainTest.java
@@ -5,7 +5,13 @@ import org.junit.Test;
 public class MainTest {
 
     @Test
-    public int test(){
+    public int test() {
+        return 0;
+    }
+
+    @Test
+    public int test2() {
         return 1;
     }
 }
+
