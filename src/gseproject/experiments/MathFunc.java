9c1f5219598638f9f74d4275e88e3b01fb9b65a1
SmartMASON <gseteamproject@gmail.com>
2016-10-16 12:21:34 +0300

    Add files via upload

---
+++ src/gseproject/experiments/MathFunc.java
@@ -0,0 +1,29 @@
+package gseproject.experiments;
+
+public class MathFunc {
+	int calls;
+
+    public int getCalls() {
+        return calls;
+    }
+
+    public long factorial(int number) {
+        calls++;
+
+        if (number < 0)
+            throw new IllegalArgumentException();
+
+        long result = 1;
+        if (number > 1) {
+            for (int i = 1; i <= number; i++)
+                result = result * i;
+        }
+
+        return result;
+    }
+
+    public long plus(int num1, int num2) {
+        calls++;
+        return num1 + num2;
+    }
+}
