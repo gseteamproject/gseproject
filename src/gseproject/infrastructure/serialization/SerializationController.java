110f20d58300f21d55a80bb791ff7b3d071a3c74
Risthart <razzak66@bk.ru>
2016-10-19 01:49:12 +0300

    SerializationController fixed, some refactoring done

---
+++ src/gseproject/infrastructure/serialization/SerializationController.java
@@ -0,0 +1,89 @@
+package gseproject.infrastructure.serialization;
+
+import gseproject.infrastructure.contracts.IContract;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+
+public class ContractsSerializationController {
+
+    public static final ContractsSerializationController Instance = new ContractsSerializationController();
+
+    private static final Charset CHARSET = StandardCharsets.ISO_8859_1;
+
+    private HashMap<Class<?>, CustomSerializers> serializationDictionary;
+
+    public ContractsSerializationController(){
+        serializationDictionary = new HashMap<>();
+    }
+
+    public <T> void RegisterSerializator(Class<T> serializableClass, IWriter writer, IReader reader) {
+        CustomSerializers<T> streams = new CustomSerializers();
+        streams.writer = writer;
+        streams.reader = reader;
+
+        /*if (serializationDictionary.containsKey(serializableClass))
+            throw new Exception(serializableClass.toString() + " already in serialization dictionary!");*/
+
+        serializationDictionary.put(serializableClass, streams);
+    }
+
+
+    //todo: fix exceptions handling
+    public <T> T Deserialize(Class<T> deserializableClass, String data){
+        //todo: throw exception if null
+        CustomSerializers serializers = serializationDictionary.get(deserializableClass);
+        if (serializers == null){
+            return null;
+        }
+        IReader<T> reader = serializers.reader;
+
+        T resultObject = null;
+
+        try (ByteArrayInputStream bufferedStream = new ByteArrayInputStream(data.getBytes(CHARSET))){
+            try (DataInputStream dataInputStream = new DataInputStream(bufferedStream)) {
+                resultObject = reader.read(dataInputStream);
+            }
+            catch (IOException e) {
+                //todo: log?
+            }
+        }
+        catch (IOException b){
+            //todo: log?
+        }
+
+        return resultObject;
+    }
+
+    public <T> String Serialize(T data){
+        Class serializableClass = data.getClass();
+
+        //todo: throw exception if null
+        CustomSerializers serializers = serializationDictionary.get(serializableClass);
+        if (serializers == null){
+            return null;
+        }
+        IWriter<T> writer = serializers.writer;
+
+        String resultString = null;
+        try(ByteArrayOutputStream byteArray = new ByteArrayOutputStream()) {
+            try (DataOutputStream dataOutputStream = new DataOutputStream(byteArray)) {
+                writer.write(data, dataOutputStream);
+                resultString = new String(byteArray.toByteArray(), CHARSET);
+            }
+            catch (IOException e) {
+            }
+        }
+        catch (IOException e){
+        }
+
+        return resultString;
+    }
+
+    class CustomSerializers<T>{
+        public IWriter<T> writer;
+        public IReader<T> reader;
+    }
+}
