d6deb34500c733250eaa9b638426fdea13fc9d72
Tobias Meyer <meyer.tobias96@gmail.com>
2016-11-02 14:28:54 +0100

    removed experiments, because its old.

--- src/gseproject/experiments/Main.java
+++ src/gseproject/experiments/Main.java
@@ -6,11 +6,9 @@ public class Main {
     public static void main(String[] p_args) {
 	String[] parameters = new String[2];
 	parameters[0] = "-gui";
+	/*
 	parameters[1] = "cleaningFloor:gseproject.experiments.floor.CleaningFloor;";
-	parameters[1] += "testRobot1:gseproject.experiments.TestBlockRobot;";
-	parameters[1] += "testRobot2:gseproject.experiments.TestBlockRobot;";
-	parameters[1] += "testRobot3:gseproject.experiments.TestBlockRobot;";
-	parameters[1] += "testRobot4:gseproject.experiments.TestBlockRobot;";
+	*/
 	Boot.main(parameters);
     }
 }
