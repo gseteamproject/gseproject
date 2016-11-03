3229b398ed07f42ef7a99732f0b74a2c5ee21804
Tobias Meyer <meyer.tobias96@gmail.com>
2016-11-03 08:52:28 +0100

    added testsuite, to make adding tests easier

---
+++ src/gseproject/tests/PassiveTests.java
@@ -0,0 +1,15 @@
+package gseproject.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import gseproject.passive.SourcePalleteCommunicatorTest;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+    SourcePalleteCommunicatorTest.class,
+ })
+
+public class PassiveTests {   
+} 
