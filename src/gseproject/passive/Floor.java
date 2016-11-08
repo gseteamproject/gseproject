8e76eb7af82603df44b35cba9324e0cded44670b
Tobias Meyer <meyer.tobias96@gmail.com>
2016-11-08 11:08:30 +0100

    refactored floor

--- src/gseproject/passive/Floor.java
+++ src/gseproject/passive/Floor.java
@@ -2,55 +2,10 @@ package gseproject.passive;
 
 import gseproject.core.Block;
 
-public class Floor implements IFloorLanguage, IStationLanguage {
-
-	@Override
-	public boolean hasBlock() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean isOccupied() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public Boolean giveBlock(Block block) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public Boolean iOccuppy() {
-		// TODO Auto-generated method stub
-		return true;
-	}
-
-	@Override
-	public void iLeave() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public Block takeBlock() {
-		// TODO Auto-generated method stub
-		return null;
-
-	}
-
-	@Override
-	public boolean hasFinishedBlock() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean isFinishedBlock() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
+public abstract class Floor {
+    protected boolean hasBlock;
+    protected boolean isOccupied;
+    protected boolean hasFinishedBlock;
+    
+    
 }
