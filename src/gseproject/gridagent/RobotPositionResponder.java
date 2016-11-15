a5c49f63433a3f8cb5641243641fbb63ef29b693
Risthart <razzak66@bk.ru>
2016-11-15 22:59:20 +0300

    Testing GUI vs Robot (lol) fixed

--- src/gseproject/gridagent/RobotPositionResponder.java
+++ src/gseproject/gridagent/RobotPositionResponder.java
@@ -1,102 +1,95 @@
 package gseproject.gridagent;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import gseproject.core.Direction;
 import gseproject.core.grid.Grid;
 import gseproject.core.grid.Position;
 import gseproject.core.grid.SpaceType;
 import gseproject.infrastructure.contracts.RobotStateContract;
 import gseproject.infrastructure.serialization.SerializationController;
-import gseproject.infrastructure.serialization.robot.RobotStateReader;
-import gseproject.infrastructure.serialization.robot.RobotStateWriter;
 import gseproject.robot.domain.RobotState;
-import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
-import javafx.util.Pair;
 
 public class RobotPositionResponder extends CyclicBehaviour {
 
-    private static final long serialVersionUID = -639699365150760529L;
-    private static SerializationController sc = SerializationController.Instance;
-    private MessageTemplate msgTemplate;
-    private Grid grid;
+	private static final long serialVersionUID = -639699365150760529L;
+	private static SerializationController sc = SerializationController.Instance;
+	private MessageTemplate msgTemplate;
+	private Grid grid;
 
-    public RobotPositionResponder(Agent a, MessageTemplate msgTemplate, Grid grid) {
-	super(a);
-	this.msgTemplate = msgTemplate;
-	this.grid = grid;
-    }
-
-    public RobotPositionResponder(Agent a, Grid grid) {
-	super(a);
-	msgTemplate = MessageTemplate.MatchPerformative(ACLMessage.INFORM);
-	this.grid = grid;
-    }
-
-    @Override
-    public void action() {
-	ACLMessage msg = myAgent.blockingReceive(msgTemplate);
-	ACLMessage reply = msg.createReply();
-	reply.setPerformative(ACLMessage.INFORM);
-	RobotStateContract robotStateContract = sc.Deserialize(RobotStateContract.class, msg.getContent());
-	System.out.println("state of robot: " + robotStateContract.toString());
-	reply.setContent(sc.Serialize(updateRobotState(robotStateContract)));
-	myAgent.send(reply);
-    }
-
-    private RobotState updateRobotState(RobotStateContract state) {
-	RobotState newState = new RobotState();
-	
-	newState.isCarryingBlock = state.isCarryingBlock;
-	newState.direction = state.direction;
-	newState.goal = state.goal;
-	if (!canStickDirection(state.position, state.direction)) {
-	    newState.direction = changeDirection(state.direction);
+	public RobotPositionResponder(Agent a, MessageTemplate msgTemplate, Grid grid) {
+		super(a);
+		this.msgTemplate = msgTemplate;
+		this.grid = grid;
 	}
-	newState.position = getNextPosition(state.position, state.direction);
-	return newState;
-    }
 
-    private Position getNextPosition(Position start, Direction direction) {
-	switch (direction) {
-	case NORTH:
-	    return new Position(start.getX(), start.getY() - 1);
-	case SOUTH:
-	    return new Position(start.getX(), start.getY() + 1);
-	case EAST:
-	    return new Position(start.getX() + 1, start.getY());
-	case WEST:
-	    return new Position(start.getX() - 1, start.getY());
+	public RobotPositionResponder(Agent a, Grid grid) {
+		super(a);
+		msgTemplate = MessageTemplate.MatchPerformative(ACLMessage.INFORM);
+		this.grid = grid;
 	}
-	return start;
-    }
 
-    private boolean canStickDirection(Position start, Direction direction) {
-	Position newPosition = getNextPosition(start, direction);
-	if (!grid.get(newPosition.getX(), newPosition.getY()).equals(SpaceType.NO_TRACK)) {
-	    return true;
-	} else {
-	    return false;
+	@Override
+	public void action() {
+		ACLMessage msg = myAgent.blockingReceive(msgTemplate);
+		ACLMessage reply = msg.createReply();
+		reply.setPerformative(ACLMessage.INFORM);
+		RobotStateContract robotStateContract = sc.Deserialize(RobotStateContract.class, msg.getContent());
+		System.out.println("state of robot: " + robotStateContract.toString());
+		reply.setContent(sc.Serialize(updateRobotState(robotStateContract)));
+		myAgent.send(reply);
 	}
-    }
 
-    private Direction changeDirection(Direction direction) {
-	switch (direction) {
-	case NORTH:
-	    return Direction.EAST;
-	case SOUTH:
-	    return Direction.WEST;
-	case EAST:
-	    return Direction.SOUTH;
-	case WEST:
-	    return Direction.NORTH;
+	private RobotStateContract updateRobotState(RobotStateContract state) {
+		RobotStateContract newState = new RobotStateContract();
+
+		newState.isCarryingBlock = state.isCarryingBlock;
+		newState.direction = state.direction;
+		newState.goal = state.goal;
+		if (!canStickDirection(state.position, state.direction)) {
+			newState.direction = changeDirection(state.direction);
+		}
+		newState.position = getNextPosition(state.position, state.direction);
+		return newState;
 	}
-	return direction;
-    }
+
+	private Position getNextPosition(Position start, Direction direction) {
+		switch (direction) {
+			case NORTH:
+				return new Position(start.getX(), start.getY() - 1);
+			case SOUTH:
+				return new Position(start.getX(), start.getY() + 1);
+			case EAST:
+				return new Position(start.getX() + 1, start.getY());
+			case WEST:
+				return new Position(start.getX() - 1, start.getY());
+		}
+		return start;
+	}
+
+	private boolean canStickDirection(Position start, Direction direction) {
+		Position newPosition = getNextPosition(start, direction);
+		if (!grid.get(newPosition.getX(), newPosition.getY()).equals(SpaceType.NO_TRACK)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	private Direction changeDirection(Direction direction) {
+		switch (direction) {
+			case NORTH:
+				return Direction.EAST;
+			case SOUTH:
+				return Direction.WEST;
+			case EAST:
+				return Direction.SOUTH;
+			case WEST:
+				return Direction.NORTH;
+		}
+		return direction;
+	}
 
 }
