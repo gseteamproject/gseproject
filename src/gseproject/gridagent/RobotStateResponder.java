45505e14531b15fe31a1554ed01a57443875d375
Tobias Meyer <meyer.tobias96@gmail.com>
2016-11-15 22:51:25 +0100

    GridAgent -> RobotAgent communication added

---
+++ src/gseproject/gridagent/RobotStateResponder.java
@@ -0,0 +1,51 @@
+package gseproject.gridagent;
+
+import gseproject.core.grid.Grid;
+import gseproject.infrastructure.contracts.RobotStateContract;
+import gseproject.infrastructure.serialization.SerializationController;
+import jade.core.Agent;
+import jade.core.behaviours.DataStore;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import jade.proto.SimpleAchieveREResponder;
+
+public class RobotStateResponder extends SimpleAchieveREResponder {
+    private static final long serialVersionUID = -6024009980616790516L;
+    private Grid grid;
+    private static SerializationController sc = SerializationController.Instance;
+
+    public RobotStateResponder(Agent a, MessageTemplate mt, Grid grid) {
+	super(a, mt);
+	this.grid = grid;
+    }
+
+    public RobotStateResponder(Agent a, MessageTemplate mt, DataStore store, Grid grid) {
+	super(a, mt, store);
+	this.grid = grid;
+    }
+
+    protected ACLMessage prepareResponse(ACLMessage request) {
+	return request;
+    }
+
+    protected ACLMessage prepareResultNotification(ACLMessage request, ACLMessage response) {
+	RobotStateContract state = sc.Deserialize(RobotStateContract.class, request.getContent());
+	updateGrid(state);
+	sendNewGridToGUI();
+	System.out.println(request.getSender() + ":" + state.toString());
+	ACLMessage agree = request.createReply();
+	return agree;
+    }
+
+    private void updateGrid(RobotStateContract state) {
+
+    }
+
+    private void sendNewGridToGUI() {
+
+    }
+
+    public Grid getGrid() {
+	return this.grid;
+    }
+}
