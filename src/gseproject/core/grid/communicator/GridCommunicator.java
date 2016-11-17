ab29c09b20f4e053f8399f8a8e44eaa5c2bcbf3d
Tobias Meyer <meyer.tobias96@gmail.com>
2016-11-17 14:20:28 +0100

    changed files for self defined protocol

--- src/gseproject/core/grid/communicator/GridCommunicator.java
+++ src/gseproject/core/grid/communicator/GridCommunicator.java
@@ -12,56 +12,64 @@ import jade.core.AID;
 import jade.lang.acl.ACLMessage;
 
 public class GridCommunicator implements IGridCommunicator {
-	private SerializationController sc = SerializationController.Instance;
-	private GridAgent gridAgent;
-	private AID GUI_AID = new AID("GUI", AID.ISLOCALNAME);
+    private SerializationController sc = SerializationController.Instance;
+    private GridAgent gridAgent;
+    private AID GUI_AID = new AID("GUI", AID.ISLOCALNAME);
 
-	public GridCommunicator(GridAgent gridAgent) {
-		this.gridAgent = gridAgent;
-		initiateSerialization();
-	}
+    public GridCommunicator(GridAgent gridAgent) {
+	this.gridAgent = gridAgent;
+	initiateSerialization();
+    }
 
-	private void initiateSerialization() {
-		RobotStateWriter writer = new RobotStateWriter();
-		RobotStateReader reader = new RobotStateReader();
-		
-		sc.RegisterSerializator(RobotStateContract.class, writer, reader);
-		
-		GridWriter gridWriter = new GridWriter();
-        GridReader gridReader = new GridReader();
-        sc.RegisterSerializator(GridContract.class, gridWriter, gridReader);
-	}
-	
-	@Override
-	public ACLMessage agreeRobot(ACLMessage messageFromRobot) {
-		ACLMessage reply = messageFromRobot.createReply();
-		reply.setPerformative(ACLMessage.AGREE);
-		return reply;
-	}
+    private void initiateSerialization() {
+	RobotStateWriter writer = new RobotStateWriter();
+	RobotStateReader reader = new RobotStateReader();
 
-	@Override
-	public ACLMessage sendInitialGrid(ACLMessage messageFromGUI) {
-		ACLMessage reply = messageFromGUI.createReply();
-		reply.setPerformative(ACLMessage.INFORM);
-		if(gridAgent.getGrid() == null){
-			System.out.println("init failed");
-		}
-		reply.setContent(sc.Serialize(new GridContract(gridAgent.getGrid())));
-		return reply;
-	}
+	sc.RegisterSerializator(RobotStateContract.class, writer, reader);
 
-	@Override
-	public void sendRobotStateToGUI(RobotStateContract robotState) {
+	GridWriter gridWriter = new GridWriter();
+	GridReader gridReader = new GridReader();
+	sc.RegisterSerializator(GridContract.class, gridWriter, gridReader);
+    }
 
-	}
+    @Override
+    public ACLMessage agreeRobot(ACLMessage messageFromRobot) {
+	ACLMessage reply = messageFromRobot.createReply();
+	reply.setPerformative(ACLMessage.AGREE);
+	return reply;
+    }
 
-	public GridAgent getGridAgent() {
-		return this.gridAgent;
+    @Override
+    public ACLMessage sendInitialGrid(ACLMessage messageFromGUI) {
+	ACLMessage reply = messageFromGUI.createReply();
+	reply.setPerformative(ACLMessage.INFORM);
+	if (gridAgent.getGrid() == null) {
+	    System.out.println("init failed");
 	}
+	reply.setContent(sc.Serialize(new GridContract(gridAgent.getGrid())));
+	return reply;
+    }
 
-	@Override
-	public AID GUI_AID() {
-		return this.GUI_AID;
-	}
+    public GridAgent getGridAgent() {
+	return this.gridAgent;
+    }
+
+    @Override
+    public AID GUI_AID() {
+	return this.GUI_AID;
+    }
+
+    @Override
+    public RobotStateContract parseRobotStateFromMessage(ACLMessage messageFromRobot) {
+	return sc.Deserialize(RobotStateContract.class, messageFromRobot.getContent());
+    }
+
+    @Override
+    public ACLMessage sendRobotStateToGUI(RobotStateContract robotState) {
+	ACLMessage message = new ACLMessage(ACLMessage.INFORM);
+	message.addReceiver(GUI_AID);
+	message.setContent(sc.Serialize(robotState));
+	return message;
+    }
 
 }
--- src/gseproject/core/grid/communicator/RobotStateResponder.java
+++ src/gseproject/core/grid/communicator/RobotStateResponder.java
@@ -1,39 +1,42 @@
-package gseproject.core.grid;
+package gseproject.core.grid.communicator;
 
-import gseproject.core.grid.communicator.IGridCommunicator;
+import gseproject.infrastructure.contracts.RobotStateContract;
 import jade.core.Agent;
 import jade.core.behaviours.DataStore;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
 import jade.proto.SimpleAchieveREResponder;
 
-public class StateResponder extends SimpleAchieveREResponder {
-	private static final long serialVersionUID = -6024009980616790516L;
-	private IGridCommunicator communicator;
+public class RobotStateResponder extends SimpleAchieveREResponder {
+    private static final long serialVersionUID = -6024009980616790516L;
+    private IGridCommunicator communicator;
+    private RobotStateContract currStateContract;
 
-	public StateResponder(Agent a, MessageTemplate mt, IGridCommunicator communicator) {
-		super(a, mt);
-		this.communicator = communicator;
-	}
+    public RobotStateResponder(Agent a, MessageTemplate mt, IGridCommunicator communicator) {
+	super(a, mt);
+	this.communicator = communicator;
+    }
 
-	public StateResponder(Agent a, MessageTemplate mt, DataStore store, IGridCommunicator communicator) {
-		super(a, mt, store);
-		this.communicator = communicator;
-	}
+    public RobotStateResponder(Agent a, MessageTemplate mt, DataStore store, IGridCommunicator communicator) {
+	super(a, mt, store);
+	this.communicator = communicator;
+    }
 
-	public StateResponder(Agent a, MessageTemplate mt, DataStore store) {
-		super(a, mt, store);
-	}
+    public RobotStateResponder(Agent a, MessageTemplate mt, DataStore store) {
+	super(a, mt, store);
+    }
 
-	protected ACLMessage prepareResultNotification(ACLMessage request, ACLMessage response) {
-		if(!request.getSender().equals(communicator.GUI_AID())){
-			//reply to Robot
-			return communicator.agreeRobot(request);
-		} else {
-			System.out.println("message from GUI");
-			//reply to GUI
-			return communicator.sendInitialGrid(request);
-		}
+    protected ACLMessage prepareResponse(ACLMessage request) {
+	this.currStateContract = communicator.parseRobotStateFromMessage(request);
+	if(currStateContract == null){
+	    System.out.println("wtf");
 	}
+	System.out.println(this.currStateContract);
+	return communicator.agreeRobot(request);
+    }
+
+    protected ACLMessage prepareResultNotification(ACLMessage request, ACLMessage response) {
+	return communicator.agreeRobot(request);
+    }
 
 }
