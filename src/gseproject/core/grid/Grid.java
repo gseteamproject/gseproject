0a1d5395cbbd01efb92911746f6ed457ba0c283e
Steffen Teichmann <steffen.teichmann@hs-emden-leer.de>
2016-11-17 12:32:47 +0100

    Fixed #16

--- src/gseproject/core/grid/Grid.java
+++ src/gseproject/core/grid/Grid.java
@@ -37,6 +37,32 @@ public class Grid implements Serializable {
 	public SpaceType[][] getSpaces() {
 		return spaces;
 	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		
+		if (obj == null) {
+			return false;
+		}
+		
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		
+		Grid grid = (Grid) obj;
+		for (int x = 0; x < grid.getWidth(); x++) {
+			for (int y = 0; y < grid.getHeight(); y++) {
+				if (this.spaces[x][y] != grid.get(x, y)) {
+					return false;
+				}
+			}
+		}
+		
+		return true;
+	}
 
 	public static class GridBuilder {
 		private final int width;
@@ -46,7 +72,7 @@ public class Grid implements Serializable {
 		public GridBuilder(int width, int height) {
 			this.width = width;
 			this.height = height;
-			this.spaces = new SpaceType[width][width];
+			this.spaces = new SpaceType[width][height];
 			for (int x = 0; x < width; x++) {
 				for (int y = 0; y < height; y++) {
 					spaces[x][y] = SpaceType.NO_TRACK;
