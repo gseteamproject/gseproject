8e73831f4d159d6250bddf575c7b3a6aceeca1c0
admin <bonobogit@cjakobi.de>
2016-11-18 22:55:02 +0100

    ....

--- src/gseproject/tests/AllTests.java
+++ src/gseproject/tests/AllTests.java
@@ -3,10 +3,6 @@ package gseproject.tests;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-import gseproject.tests.unitTests.GoalpaletteTest;
-import gseproject.tests.unitTests.SourcepaletteTest;
-import gseproject.tests.unitTests.StationCommunicatorSourcepaletteTest;
-
 @RunWith(Suite.class)
 
 @Suite.SuiteClasses({
