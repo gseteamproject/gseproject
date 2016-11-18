3cda04a559d9e069ee05021ae8f1a5cb27188673
Tobias Meyer <meyer.tobias96@gmail.com>
2016-11-18 14:29:35 +0100

    added skeleton palleteCommunicator that lukas will implement

---
+++ src/gseproject/passive/communicator/PalleteCommunicator.java
@@ -0,0 +1,31 @@
+package gseproject.passive.communicator;
+
+import jade.lang.acl.ACLMessage;
+
+public class PalleteCommunicator implements IStationCommunicator{
+
+    @Override
+    public ACLMessage handleServiceTypeRequest(ACLMessage serviceTypeRequest) {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public ACLMessage handleAction(ACLMessage actionDone) {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public ACLMessage notifyGrid() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public ACLMessage notifyRobot() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+}
