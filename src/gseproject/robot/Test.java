855d9c23a3a3ee80729bb2280a24f71685567587
kinnder <kinnder@mail.ru>
2017-10-05 01:32:54 +0300

    delete unnecessary classes

--- src/gseproject/robot/Test.java
+++
@@ -1,8 +0,0 @@
-package gseproject.robot;
-
-public class Test {
-
-    public static void main(String[] args){
-        RobotAgent agent = new RobotAgent();
-    }
-}
--- src/gseproject/tests/serialization/Test.java
+++
@@ -1,8 +0,0 @@
-package gseproject.tests.serialization;
-
-public class Test {
-
-    public static void main(String[] args){
-        RobotAgent agent = new RobotAgent();
-    }
-}
